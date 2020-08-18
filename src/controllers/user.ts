7e810f2e0ed4b5fce78d10939b4103f0ec4f467c
jonbos-palmer <jbos@thepalmergroup.com>
2020-08-18 08:20:33 -0500

    stub user signup

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,9 +1,24 @@
 import { Request, Response, NextFunction, RequestHandler } from "express";
+import { User, UserDocument } from "../models/User";
 
 export const signup: RequestHandler = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  res.json({ body: req.body });
+  const { email, password } = req.body;
+
+  // Search for existing user
+
+  // Create user w/ username and password
+
+  const user: UserDocument = new User({ email, password });
+
+  user.save((err, user) => {
+    if (err) {
+      res.json({ err });
+    } else {
+      res.json({ msg: "Signup Successful!" });
+    }
+  });
 };
